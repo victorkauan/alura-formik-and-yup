dc1da759025fd90f5561609b1be4cbe3bae52377
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-06 22:14:18 -0300

    :recycle: refactor: Refactor radio components with Formik

--- src/componentes/Radio/GrupoRadio.jsx
+++ src/componentes/Radio/GrupoRadio.jsx
@@ -1,19 +1,22 @@
+import { useFormikContext } from "formik";
 import Radio from "./Radio";
 
-const GrupoRadio = ({ opcoes, valor, onChange }) => {
-  return (
-    <div>
-      {opcoes.map((option) => (
-        <Radio
-          key={option.valor}
-          valor={option.valor}
-          label={option.label}
-          checked={option.valor === valor}
-          onClick={() => onChange(option.valor)}
-        />
-      ))}
-    </div>
-  );
+const GrupoRadio = ({ opcoes, name }) => {
+    const { values } = useFormikContext();
+
+    return (
+        <div>
+            {opcoes.map((option) => (
+                <Radio
+                    name={name}
+                    key={option.valor}
+                    value={option.label}
+                    label={option.label}
+                    checked={option.label === values[name]}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default GrupoRadio;
--- src/componentes/Radio/Radio.jsx
+++ src/componentes/Radio/Radio.jsx
@@ -1,49 +1,62 @@
 import styled from "@emotion/styled";
-
+import { Field } from "formik";
 
 const LabelEstilizada = styled.label`
     display: flex;
     align-items: center;
     font-size: 16px;
-    margin-left: ${props => props.theme.espacamentos.xs};
+    margin-left: ${(props) => props.theme.espacamentos.xs};
     cursor: pointer;
-    padding: ${props => props.theme.espacamentos.xs};
+    padding: ${(props) => props.theme.espacamentos.xs};
     &:hover {
         &:before {
-            border-color: ${props => (props.theme.cores.focus)};
+            border-color: ${(props) => props.theme.cores.focus};
         }
     }
     &:before {
-        content: '';
+        content: "";
         height: 20px;
         width: 20px;
         border-radius: 50%;
-        border: 2px solid ${props => props.theme.cores.neutras.a};
+        border: 2px solid ${(props) => props.theme.cores.neutras.a};
         border-color: inherit;
         display: flex;
         align-items: center;
         justify-content: center;
         margin-right: 10px;
         cursor: pointer;
-        background-color: ${props => (props.checked ? props.theme.cores.primarias.b : props.theme.cores.neutras.c)};
-        border-color: ${props => (props.checked ? props.theme.cores.primarias.b : 'inherit')};
+        background-color: ${(props) =>
+            props.checked
+                ? props.theme.cores.primarias.b
+                : props.theme.cores.neutras.c};
+        border-color: ${(props) =>
+            props.checked ? props.theme.cores.primarias.b : "inherit"};
     }
 `;
 
-const InputEscondido = styled.input`
+const InputEscondido = styled(Field)`
     appearance: none;
     opacity: 0;
     &:focus + label {
         border-radius: 16px;
         border: 1px solid;
-        border-color: ${props => props.theme.cores.focus};
+        border-color: ${(props) => props.theme.cores.focus};
     }
-`
+`;
 
-const Radio = ({ valor, label, checked, onClick }) => (
+const Radio = ({ label, checked, name, value }) => (
     <>
-        <InputEscondido type="radio" valor={valor} checked={checked} onChange={onClick} id={`radio-customizado-${valor}`} />
-        <LabelEstilizada htmlFor={`radio-customizado-${valor}`} checked={checked}>
+        <InputEscondido
+            type="radio"
+            name={name}
+            value={value}
+            checked={checked}
+            id={`radio-customizado-${value}`}
+        />
+        <LabelEstilizada
+            htmlFor={`radio-customizado-${value}`}
+            checked={checked}
+        >
             {label}
         </LabelEstilizada>
     </>
