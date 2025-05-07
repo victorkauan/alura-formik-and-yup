8736521115134d711f4581bb83c299ed7f6c1cd6
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-06 21:55:30 -0300

    :recycle: refactor: Refactor text field component with Formik

--- src/componentes/CampoTexto/CampoTexto.jsx
+++ src/componentes/CampoTexto/CampoTexto.jsx
@@ -1,4 +1,5 @@
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
+import { ErrorMessage, Field } from "formik";
 
 const LabelEstilizada = styled.label`
     display: block;
@@ -7,32 +8,37 @@ const LabelEstilizada = styled.label`
     font-weight: 400;
     font-size: 20px;
     line-height: 24px;
-`
+`;
 
-const InputEstilizado = styled.input`
+const InputEstilizado = styled(Field)`
     display: block;
     width: 100%;
     box-sizing: border-box;
-    margin-top: ${props => props.theme.espacamentos.xs};
-    margin-bottom: ${props => props.theme.espacamentos.s};
-    background: ${props => props.theme.cores.branco};
+    margin-top: ${(props) => props.theme.espacamentos.xs};
+    margin-bottom: ${(props) => props.theme.espacamentos.s};
+    background: ${(props) => props.theme.cores.branco};
     border: 1px solid;
-    border-color: ${props => props.theme.cores.neutras.a};
-    border-radius: ${props => props.theme.espacamentos.s};
-    padding: 0 ${props => props.theme.espacamentos.s};
+    border-color: ${(props) => props.theme.cores.neutras.a};
+    border-radius: ${(props) => props.theme.espacamentos.s};
+    padding: 0 ${(props) => props.theme.espacamentos.s};
     height: 40px;
     font-weight: 400;
     font-size: 14px;
     line-height: 17px;
-`
+`;
 
-export const CampoTexto = ({ titulo, valor, onChange, tipo = 'text' }) => {
-    return (<LabelEstilizada>
-        {titulo}
-        <InputEstilizado
-            value={valor}
-            onChange={evento => onChange(evento.target.value)}
-            type={tipo}
-        />
-    </LabelEstilizada>)
-}
+export const CampoTexto = ({ titulo, ...props }) => {
+    return (
+        <LabelEstilizada>
+            {titulo}
+            <InputEstilizado {...props} />
+            <ErrorMessage name={props.name}>
+                {(mensagens) => (
+                    <div style={{ color: "red" }}>
+                        {mensagens}
+                    </div>
+                )}
+            </ErrorMessage>
+        </LabelEstilizada>
+    );
+};
